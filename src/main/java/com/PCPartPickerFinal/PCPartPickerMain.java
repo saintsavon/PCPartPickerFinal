a8eaf05e9e056cece3ea81aac82f8027dbe7897f
saintsavon <jaylensavon@gmail.com>
2020-10-19 16:32:16 -0400

    Got functionality working in response to user

--- src/main/java/com/PCPartPickerFinal/PCPartPickerMain.java
+++ src/main/java/com/PCPartPickerFinal/PCPartPickerMain.java
@@ -1,6 +1,7 @@
 package com.PCPartPickerFinal;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Scanner;
 import java.io.InputStreamReader;
 import java.io.FileInputStream;
 
@@ -9,26 +10,43 @@ public class PCPartPickerMain {
 	public static void main(String[] args) {
 
 		ObjectMapper om = new ObjectMapper();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Hello! Thank you for using PCParPicker to build your dream system today! (Please don't sue me PCPartPicker)");
+		System.out.println("Let's begin by selecting your CPU brand: intel or amd?");
+		String cpu_choice = scan.nextLine();
+		
+		//try {
+		//	PCPartsList ppl = om.readValue(new InputStreamReader(new FileInputStream("parts.json")), PCPartsList.class);
+			//System.out.println(ppl);
+			//System.out.println(ppl.getCpu().getBrands()[1].getBrandName());
+			
+		//	for (CPUBrand brand: ppl.getCpu().getBrands()) {
+		//		if (brand.getBrandName().equals("intel")) {
+		//			System.out.println(brand);
+		//		} else if (brand.getBrandName().equals("amd")){
+		//			System.out.println(brand);
+		//		} else {
+		//			System.out.checkError();
+		//		}
+		//	}
+			
+		//} catch (Exception e) {
+		//	e.printStackTrace();
+		//}
+		
 		
 		try {
 			PCPartsList ppl = om.readValue(new InputStreamReader(new FileInputStream("parts.json")), PCPartsList.class);
-			System.out.println(ppl);
-			System.out.println(ppl.getCpu().getBrands()[1].getBrandName());
 			
 			for (CPUBrand brand: ppl.getCpu().getBrands()) {
-				if (brand.getBrandName().equals("intel")) {
+				if (brand.getBrandName().equals(cpu_choice)) {
 					System.out.println(brand);
-				} else if (brand.getBrandName().equals("amd")){
-					System.out.println(brand);
-				} else {
-					System.out.checkError();
 				}
 			}
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 
 }
