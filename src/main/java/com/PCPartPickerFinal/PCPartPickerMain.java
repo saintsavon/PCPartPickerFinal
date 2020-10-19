de0bb9a6c75bcb4a91b8357cae2bb1789f98e2d4
saintsavon <jaylensavon@gmail.com>
2020-10-19 16:54:35 -0400

    Attempting to set up user interaction and storing selections

--- src/main/java/com/PCPartPickerFinal/PCPartPickerMain.java
+++ src/main/java/com/PCPartPickerFinal/PCPartPickerMain.java
@@ -1,7 +1,7 @@
 package com.PCPartPickerFinal;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Scanner;
+import java.util.*;
 import java.io.InputStreamReader;
 import java.io.FileInputStream;
 
@@ -15,6 +15,7 @@ public class PCPartPickerMain {
 		System.out.println("Hello! Thank you for using PCParPicker to build your dream system today! (Please don't sue me PCPartPicker)");
 		System.out.println("Let's begin by selecting your CPU brand: intel or amd?");
 		String cpu_choice = scan.nextLine();
+		List parts_list = new ArrayList(); 
 		
 		//try {
 		//	PCPartsList ppl = om.readValue(new InputStreamReader(new FileInputStream("parts.json")), PCPartsList.class);
@@ -42,11 +43,30 @@ public class PCPartPickerMain {
 			for (CPUBrand brand: ppl.getCpu().getBrands()) {
 				if (brand.getBrandName().equals(cpu_choice)) {
 					System.out.println(brand);
+					parts_list.add(brand); // this actually needs to be the individual CPU name
+					System.out.println(parts_list); // move this and line above to after CPU selection process
+				} else {
+					System.out.println("Please select either intel or amd"); // bug here that always prints at least once
 				}
-			}
+			} 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		System.out.println("Please select your desired compatible motherboard:");
+		
+		try {
+			PCPartsList ppl = om.readValue(new InputStreamReader(new FileInputStream("parts.json")), PCPartsList.class);
+
+			System.out.println(ppl.getMobo().getBrands());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Please select your desired compatible motherboard:");
+
+		
 	}
 
 }
